2094904196fd458764e82cf2c0605076fd2f4089
harshvasa <49966209+harshvasa@users.noreply.github.com>
2019-05-19 22:46:30 +0530

    updated StarterApplication.java

---
+++ ParseStarterProject/src/main/java/com/parse/starter/StarterApplication.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.parse.starter;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+public class StarterApplication extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Enable Local Datastore.
+    Parse.enableLocalDatastore(this);
+
+    // Add your initialization code here
+    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+            .applicationId("d3fc1bf6b1a57e3daeecb93b4b08d7101e0d3022")
+            .clientKey("50040fa60c8f3d017056d17e2be8298bed86d28e")
+            .server("http://127.0.0.1:80/parse/")
+            .build()
+    );
+
+
+    //ParseUser.enableAutomaticUser();
+
+    ParseACL defaultACL = new ParseACL();
+    defaultACL.setPublicReadAccess(true);
+    defaultACL.setPublicWriteAccess(true);
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
